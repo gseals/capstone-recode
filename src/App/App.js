0a025cfeb16155b37c4dca3a4ff33e69de7efe8b
Gabriel Seals <sealsgj@gmail.com>
2020-06-15 13:36:39 -0500

    firebase auth and login work

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
 import './App.scss';
 
-class App extends Component {
-  testClick() {
-    console.log('test');
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import firebaseConnnection from '../helpers/data/connection';
+
+import Auth from '../components/pages/Auth/Auth';
+import AllPosts from '../components/pages/AllPosts/AllPosts';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+firebaseConnnection();
+
+class App extends React.Component {
+  state={
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
+    const { authed } = this.state;
     return (
     <div className="App">
-      <button
-        className="btn btn-danger"
-        onClick={() => this.testClick()}
-        >
-          Test
-        </button>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact component={AllPosts} authed={authed}/>
+          <PublicRoute path="/auth" exact component={Auth} authed={authed}/>
+        </Switch>
+      </Router>
     </div>
     );
   }
