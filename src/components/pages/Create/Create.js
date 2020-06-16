69aad273aeaf2abd96f41fb586d55ce6cc1e3a97
Gabriel Seals <sealsgj@gmail.com>
2020-06-16 09:07:52 -0500

    routing setup

---
+++ src/components/pages/Create/Create.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Create.scss';
+
+class Create extends React.Component {
+  render() {
+    return (
+      <div className="Create">
+        <h1>Create</h1>
+      </div>
+    );
+  }
+}
+
+export default Create;
