34ebc40bed98b8424c3b9ae7fecab36166eaeeb0
Gabriel Seals <sealsgj@gmail.com>
2020-06-17 08:31:02 -0500

    created seed data and updated routes

--- src/App/App.js
+++ src/App/App.js
@@ -54,14 +54,14 @@ class App extends React.Component {
       <Router>
         <MyNavBar authed={authed} />
         <Switch>
-          <PublicRoute path="/about" exact component={About} authed={authed}/>
+          <Route path="/about" exact component={About} authed={authed}/>
           <PublicRoute path="/auth" exact component={Auth} authed={authed}/>
           <PrivateRoute path="/about" exact component={About} authed={authed}/>
           <PrivateRoute path="/" exact component={AllPosts} authed={authed}/>
           <PrivateRoute path="/posts/all" exact component={AllPosts} authed={authed}/>
           <PrivateRoute path="/posts/create" exact component={Create} authed={authed}/>
-          <PrivateRoute path="/posts/:postId/feedback" exact component={Feedback} authed={authed}/>
-          <PrivateRoute path="/posts/:postId/update" exact component={Update} authed={authed}/>
+          <PrivateRoute path="/posts/:postPathId/feedback" exact component={Feedback} authed={authed}/>
+          <PrivateRoute path="/posts/:postPathId/update" exact component={Update} authed={authed}/>
         </Switch>
       </Router>
     </div>
